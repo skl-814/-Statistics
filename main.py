668a213a125cbfe230c40949a66ea5778df6ea7e
skl-814 <85046404+skl-814@users.noreply.github.com>
2022-01-01 20:26:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,25 +10,29 @@ git clone:
 https://github.com/skl-814/-Statistics.git
 https://gitee.com/skl-814/statistics1.git
 '''
-
+import sys
 l,ll = [],[]
 # code for collecting
+i = ""
 while i !="q" or "f":
     i  =input("Number:\n")
     try:
         l.append(float(i))
     except:
-        pass
+        if i == "f":
+            break
+        elif i == "q":
+            sys.exit()
 # end
 _x = sum(l)/len(l)
     
 for a in l:
     ll.append((a-_x)**2)
     
-_s = sum(l)/len(ll)
+_s = sum(ll)/len(ll)
 # code for printing...
-print(f"平均数：{_x}")
+print(f"平均数：\n\t{_x}")
 print(f"方差：\n\t{_s}")
 print(f"最大值:\n\t{max(l)}")
 print(f"最小值:\n\t{min(l)}")
-print(f"极差{max(l)-min(l)}")
+print(f"极差\n\t{max(l)-min(l)}")
