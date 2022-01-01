d18128391a11a6397530f7584c9a3b148c98a9f9
skl-814 <85046404+skl-814@users.noreply.github.com>
2022-01-01 20:06:14 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,99 +1,28 @@
-#!usr/bin/var python3
-#maker:SKL
-import json
+#!usr/bin/ python3
+'''
+统计程序 V3.1.1
+by skl
+https://github.com/
+'''
 
-num = " "
-numlist = []
-
-print("输入数据（回车完成)")
-while num != "":
-    num = input("\n数据:\n")
-    if num == "":
-        break
-    else:
-        try:
-            num = float(num)
-        except:
-            print("Wrong!")
-            while True:
-                print("",end="")
-        else:
-            numlist.append(num)
-
-numlist.sort()
-
-llong = len(numlist)
-
-a = 0
-for aa in numlist:
-    a = a + aa
-
-a = a/llong
-print(f"\n\n平均数：\n{a}")
-d1 = str(a)
-
-aaa = 0
-for aa in numlist:
-    ab = aa - a
-    ab = ab ** 2
-    aaa = aaa + ab
-
-f_num = aaa/llong
-print(f"\n方差：\n{f_num}")
-d2 = str(f_num)
-
-print("\n最小值:\n",min(numlist))
-d3 = str(min(numlist))
-print("\n最大值:\n",max(numlist))
-d4 = str(max(numlist))
-ccc = max(numlist)-min(numlist)
-print("\n极差:\n",ccc)
-d5 = str(ccc)
-del ccc
-numlist.sort()
-if llong%2 == 0:
-    c1 = int(llong / 2 - 1)
-    c2 = int(c1 + 1)
-
-    c1 = numlist[c1]
-    c2 = numlist[c2]
-    c3 = (c1 + c2) / 2
-    print("\n中位数：\n",c3)
-    d6 = str(c3)
-elif llong%2 != 0:
-    c1 = int(llong / 2 + 0.5 - 1)
-    c1 = numlist[c1]
-    print("\n中位数：\n",c1)
-    d6 = str(c1)
-
-
-#with open("history.json","a",) as hf:
-#    json.dump(numlist,hf)
-#    json.dump(d1,hf)
-#    json.dump(d2,hf)
-#    json.dump(d3,hf)
-#    json.dump(d4,hf)
-#    json.dump(d5,hf)
-#    json.dump(d6,hf)
-#    json.dump("____________________",hf)
-
-with open("history.txt","a") as hft:
-    hft.write(str(numlist)+"\n")
-    hft.write(d1+"\n")
-    hft.write(d2+"\n")
-    hft.write(d3+"\n")
-    hft.write(d4+"\n")
-    hft.write(d5+"\n")
-    hft.write(d6+"\n")
-    hft.write("\n")
-    hft.write("____________________")
-    hft.write("____________________")
-    hft.write("____________________")
-    hft.write("___________________")
-    hft.write("\n")
-    hft.write("\n")
-
-while True:
-    print("",end="")
-
-#     SKL make
+l,ll = [],[]
+# code for collecting
+while i !="q" or "f":
+    i  =input("Number:\n")
+    try:
+        l.append(float(i))
+    except:
+        pass
+# end
+_x = sum(l)/len(l)
+    
+for a in l:
+    ll.append((a-_x)**2)
+    
+_s = sum(l)/len(ll)
+# code for printing...
+print(f"平均数：{_x}")
+print(f"方差：\n\t{_s}")
+print(f"最大值:\n\t{max(l)}")
+print(f"最小值:\n\t{min(l)}")
+print(f"极差{max(l)-min(l)}")
